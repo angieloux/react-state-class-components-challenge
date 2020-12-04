733e1baf7cad55effd4a2253b539558f0880d3ce
Edward Deam <edwardcrdeam@gmail.com>
2020-12-04 12:36:44 +1100

    Tests for Challenge

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,26 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from './App';
 
-test('renders The basic empty template', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/basic empty template/i);
-  expect(linkElement).toBeInTheDocument();
+test('shows all songs on load', () => {
+  const { getByTestId } = render(<App />);
+  const songs = getByTestId("app").getElementsByTagName("p");
+
+  expect(songs).toHaveLength(7);
+});
+
+test('shows filters songs on search', () => {
+  const { getByRole, getByTestId } = render(<App />);
+  userEvent.type(getByRole("textbox"), "Hotel");
+
+  expect(getByTestId("app")).toHaveTextContent("Hotel California")
+});
+
+test('shows multiple matches on search', () => {
+  const { getByRole, getByTestId } = render(<App />);
+  userEvent.type(getByRole("textbox"), "d");
+
+  expect(getByTestId("app")).toHaveTextContent("Single Ladies");
+  expect(getByTestId("app")).toHaveTextContent("Run the World");
 });
