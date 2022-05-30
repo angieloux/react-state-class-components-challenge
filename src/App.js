d38b2fadb43add04f8a0639eeeecc7465d830963
ANGIELOUX <angelajohnson943@gmail.com>
2022-05-30 20:32:15 +1000

    Challenge completed

--- src/App.js
+++ src/App.js
@@ -4,19 +4,31 @@ class App extends Component {
       constructor(props){
         super(props)
         // code here
+        this.state = {
+          userInput: "",
+          songs: ["Despacito", "Summer Of 69", "Hotel California", "Single Ladies", "If I were a boy", "Run the World", "Waka Waka" ]
+        }
         // This binding is necessary to make `this` work in the callback
         this.handleClick = this.handleClick.bind(this);
         this.filterSongs = this.filterSongs.bind(this);
       }
 
-      handleClick(){
+      handleClick(e){
         // code here
+        e.preventDefeault();
       }
 
-      filterSongs(){
+      filterSongs(e){
         // code here
+        this.setState({
+          userInput: e.target.value
+        })
       }
+
       render(){
+        const filteredSongs = this.state.songs.filter(song => {
+          return song.toLowerCase().includes(this.state.userInput)
+        });
         return(
           <div data-testid="app" >
             <h1> SEARCH SONG </h1>
@@ -24,7 +36,7 @@ class App extends Component {
                     <input type="text" onChange={this.filterSongs} />
                     <input type="submit" value="Filter" />
             </form>
-            {/* list songs here */}
+            {filteredSongs.map( (song) =>  <p key={song}> {song} </p>)}
           </div>
 
         )
